517537dbfda9542ae16b062653aad406b826734a
marvin <zhiguo.xie@m2.com>
2025-05-27 14:24:09 +0800

    text2img to img2text2img

--- ai/business/image/tasks.py
+++ ai/business/image/tasks.py
@@ -9,22 +9,22 @@ workflow = CeleryWorkflow()
 @app.task
 def amz_to_ali_image(data: dict):
     data = workflow.build_payload_taskid(data)
-    return service.run_task("text2img2oss",data)
+    return service.run_task("img2text2img2oss",data)
 
 @app.task
 def amz_to_1688_image(data: dict):
     data = workflow.build_payload_taskid(data)
-    return service.run_task("text2img2oss",data)
+    return service.run_task("img2text2img2oss",data)
 
 @app.task
 def ali_to_1688_image(data: dict):
     data = workflow.build_payload_taskid(data)
-    return service.run_task("text2img2oss",data)
+    return service.run_task("img2text2img2oss",data)
 
 @app.task
 def _1688_to_1688_image(data: dict):
     data = workflow.build_payload_taskid(data)
-    return service.run_task("text2img2oss",data)
+    return service.run_task("img2text2img2oss",data)
 
 @app.task
 def ali_to_ali_image(data: dict):
